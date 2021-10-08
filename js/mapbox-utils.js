3065d63d9a1ab1043ced7bddf18e90e68b2e113f
Sledgerobby35 <sledge.robby35@gmail.com>
2021-10-08 16:24:33 -0500

    added more cards to weathermap and flushed out functionality

--- js/mapbox-utils.js
+++ js/mapbox-utils.js
@@ -1,12 +1,19 @@
 mapboxgl.accessToken = MAPBOX_TOKEN;
+
 let map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/satellite-streets-v11',
 	center: [-98.2911, 29.5480],
 	zoom: 13
 });
+
+let marker;
+mapEvent();
+
 let geocoder = setGeocoder();
 addGeocoderToMap(geocoder);
+
+
 function setGeocoder(){
 	return new MapboxGeocoder({
 		accessToken: mapboxgl.accessToken,
@@ -16,10 +23,32 @@ function setGeocoder(){
 }
 function addGeocoderToMap(geocoder){
 	map.addControl(geocoder);
+
+	geocoder.on('result', function(event){
+		console.log(event.result.place_name);
+		console.log(event);
+
+		setMarker(event.result.geometry.coordinates);
+		marker.setPopup(displayPopup(event.result.place_name));
+
+		fetchForecast(event.result.geometry.coordinates)
+	});
 }
 
-function getLngLat(){
+function setMarker(point){
+	if(!marker){
+		marker = new mapboxgl.Marker().setLngLat(point).addTo(map);
+	} else {
+		marker.setLngLat(point);
+	}
+}
+function mapEvent(){
 	map.on("click", function(event) {
-		let lngLat = [event.lngLat.lat, event.lngLat.lng];
+		setMarker(event.lngLat);
+		fetchForecast([event.lngLat.lng, event.lngLat.lat])
 		});
+}
+
+function displayPopup(textDetails){
+	return new mapboxgl.Popup().setHTML(`<p>${textDetails}</p>`).addTo(map);
 }
